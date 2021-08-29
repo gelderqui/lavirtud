e26cab878f489c06210785179302c581bffced51
Gelder Quiñónez <gelderqui@gmail.com>
2021-08-28 23:02:25 -0600

    inicio

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categoria;
+use Carbon\Carbon;
+
+class CategoriaController extends Controller
+{
+    public function index(Request $request)
+    {
+        //if (!$request->ajax()) return redirect('/');
+
+        $buscar = $request->buscar;
+        $criterio = $request->criterio;
+
+        $mytime= Carbon::now('America/Guatemala');
+        
+        if ($buscar==''){
+            $categorias = Categoria::orderBy('id', 'desc')
+            ->where('condicion','=','1')
+            ->paginate(10);
+        }
+        else{
+            $categorias = Categoria::where($criterio, 'like', '%'. $buscar . '%')
+            ->where('condicion','=','1')
+            ->orderBy('id', 'asc')
+            ->paginate(10);
+        }
+        
+        return [
+            'pagination' => [
+                'total'        => $categorias->total(),
+                'current_page' => $categorias->currentPage(),
+                'per_page'     => $categorias->perPage(),
+                'last_page'    => $categorias->lastPage(),
+                'from'         => $categorias->firstItem(),
+                'to'           => $categorias->lastItem(),
+                'towe'           => $categorias->items(),
+            ],
+            'categorias' => $categorias,
+            'fecha'=>$mytime
+        ];
+    }
+
+    public function store(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+        $categoria = new Categoria();
+        $categoria->nombre = $request->nombre;
+        $categoria->descripcion = $request->descripcion;
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+    public function update(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->nombre = $request->nombre;
+        $categoria->descripcion = $request->descripcion;
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+    public function desactivar(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->condicion = '0';
+        $categoria->save();
+    }
+
+    public function activar(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+    public function listarPdf(){
+        $categorias = Categoria::select('nombre','descripcion','condicion')->orderBy('nombre', 'asc')
+        ->where('condicion','=','1')->get();
+        //$cont=Categoria::count();
+        $cont = Categoria::where('condicion','=',1)->count();
+        
+
+        $pdf = \PDF::loadView('pdf.categoriaspdf',['categorias'=>$categorias,'cont'=>$cont])->setPaper('a4', 'portrait');
+        return $pdf->download('categorias.pdf');
+    }
+
+    public function selectCategoria(Request $request){
+        if (!$request->ajax()) return redirect('/');
+        $categorias = Categoria::where('condicion','=','1')
+        ->select('id','nombre')->orderBy('nombre', 'asc')->get();
+        return ['categorias' => $categorias];
+    }
+
+}
---
+++ app/Http/Controllers/IngresoController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Models\Ingreso;
+use App\Models\DetalleIngreso;
+
+class IngresoController extends Controller
+{
+    public function index(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+
+        $buscar = $request->buscar;
+        $criterio = $request->criterio;
+        
+        if ($buscar==''){
+            $ingresos = Ingreso::join('personas','ingresos.idproveedor','=','personas.id')
+            ->join('users','ingresos.idusuario','=','users.id')
+            ->select('ingresos.id','ingresos.tipo_comprobante','ingresos.serie_comprobante',
+            'ingresos.num_comprobante','ingresos.fecha_hora','ingresos.impuesto','ingresos.total',
+            'ingresos.estado','personas.nombre','users.usuario')
+            ->orderBy('ingresos.id', 'desc')->paginate(10);
+        }
+        else{
+            $ingresos = Ingreso::join('personas','ingresos.idproveedor','=','personas.id')
+            ->join('users','ingresos.idusuario','=','users.id')
+            ->select('ingresos.id','ingresos.tipo_comprobante','ingresos.serie_comprobante',
+            'ingresos.num_comprobante','ingresos.fecha_hora','ingresos.impuesto','ingresos.total',
+            'ingresos.estado','personas.nombre','users.usuario')
+            ->where('ingresos.'.$criterio, 'like', '%'. $buscar . '%')->orderBy('ingresos.id', 'desc')->paginate(10);
+        }
+        
+        return [
+            'pagination' => [
+                'total'        => $ingresos->total(),
+                'current_page' => $ingresos->currentPage(),
+                'per_page'     => $ingresos->perPage(),
+                'last_page'    => $ingresos->lastPage(),
+                'from'         => $ingresos->firstItem(),
+                'to'           => $ingresos->lastItem(),
+            ],
+            'ingresos' => $ingresos
+        ];
+    }
+
+    public function listarPdf(){
+        $ingresos = Ingreso::join('personas','ingresos.idproveedor','=','personas.id')
+            ->join('users','ingresos.idusuario','=','users.id')
+            ->select('ingresos.id','ingresos.tipo_comprobante','ingresos.serie_comprobante',
+            'ingresos.num_comprobante','ingresos.fecha_hora','ingresos.impuesto','ingresos.total',
+            'ingresos.estado','personas.nombre','users.usuario')
+            ->orderBy('ingresos.id', 'asc')->get();
+        $cont=Ingreso::count();
+
+        $pdf = \PDF::loadView('pdf.ingresospdf',['ingresos'=>$ingresos,'cont'=>$cont])->setPaper('a4', 'landscape');
+        return $pdf->download('ingresos.pdf');
+    }
+
+    public function pdf(Request $request,$id){
+        $ingreso = Ingreso::join('personas','ingresos.idproveedor','=','personas.id')
+        ->join('users','ingresos.idusuario','=','users.id')
+        ->select('ingresos.id','ingresos.tipo_comprobante','ingresos.serie_comprobante',
+        'ingresos.num_comprobante','ingresos.created_at','ingresos.impuesto','ingresos.total',
+        'ingresos.estado','personas.nombre','personas.tipo_documento','personas.num_documento',
+        'personas.direccion','personas.email',
+        'personas.telefono','users.usuario')
+        ->where('ingresos.id','=',$id)
+        ->orderBy('ingresos.id', 'desc')->take(1)->get();
+
+        $detalles = DetalleIngreso::join('articulos','detalle_ingresos.idarticulo','=','articulos.id')
+        ->select('detalle_ingresos.cantidad','detalle_ingresos.precio',
+        'articulos.nombre as articulo')
+        ->where('detalle_ingresos.idingreso','=',$id)
+        ->orderBy('detalle_ingresos.id', 'desc')->get();
+
+        $numingreso=Ingreso::select('num_comprobante')->where('id',$id)->get();
+        $serie=Ingreso::select('serie_comprobante')->where('id',$id)->get();
+
+        $pdf = \PDF::loadView('pdf.ingreso',['ingreso'=>$ingreso,'detalles'=>$detalles]);
+        return $pdf->download('ingreso-'.$serie[0]->serie_comprobante.'-'.$numingreso[0]->num_comprobante.'.pdf');
+
+
+    }
+
+    public function obtenerCabecera(Request $request){
+        if (!$request->ajax()) return redirect('/');
+
+        $id = $request->id;
+        $ingreso = Ingreso::join('personas','ingresos.idproveedor','=','personas.id')
+        ->join('users','ingresos.idusuario','=','users.id')
+        ->select('ingresos.id','ingresos.tipo_comprobante','ingresos.serie_comprobante',
+        'ingresos.num_comprobante','ingresos.fecha_hora','ingresos.impuesto','ingresos.total',
+        'ingresos.estado','personas.nombre','users.usuario')
+        ->where('ingresos.id','=',$id)
+        ->orderBy('ingresos.id', 'desc')->take(1)->get();
+        
+        return ['ingreso' => $ingreso];
+    }
+
+    public function obtenerDetalles(Request $request){
+        if (!$request->ajax()) return redirect('/');
+
+        $id = $request->id;
+        $detalles = DetalleIngreso::join('articulos','detalle_ingresos.idarticulo','=','articulos.id')
+        ->select('detalle_ingresos.cantidad','detalle_ingresos.precio','articulos.nombre as articulo')
+        ->where('detalle_ingresos.idingreso','=',$id)
+        ->orderBy('detalle_ingresos.id', 'desc')->get();
+        
+        return ['detalles' => $detalles];
+    }
+
+    public function store(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+       $cont=Ingreso::count();
+       $numeroComprobante=$cont+1;
+
+       $fecha= Carbon::now('America/Guatemala');
+       $mes = $fecha->month;
+       $dia = $fecha->day;
+       $año = $fecha->year;
+       $meses = array("Ene","Feb","Mar","Ab","May","Jun","Jul","Agos","Sept","Oct","Nov","Dic");
+       $mes2 = $meses[($fecha->format('n')) - 1];
+       $fechacompleta= $dia.$mes2.$año;
+
+        try{
+            DB::beginTransaction();
+
+            $mytime= Carbon::now('America/Lima');
+
+            $ingreso = new Ingreso();
+            $ingreso->idproveedor = $request->idproveedor;
+            $ingreso->idusuario = \Auth::user()->id;
+            $ingreso->tipo_comprobante = $request->tipo_comprobante;
+            //$ingreso->serie_comprobante = $request->serie_comprobante;
+            $ingreso->serie_comprobante = $fechacompleta;
+            //$ingreso->num_comprobante = $request->num_comprobante;
+            $ingreso->num_comprobante = $numeroComprobante;
+            $ingreso->fecha_hora = $mytime->toDateString();
+            $ingreso->impuesto = $request->impuesto;
+            $ingreso->total = $request->total;
+            $ingreso->estado = 'Registrado';
+            $ingreso->save();
+
+            $detalles = $request->data;//Array de detalles
+            //Recorro todos los elementos
+
+            foreach($detalles as $ep=>$det)
+            {
+                $detalle = new DetalleIngreso();
+                $detalle->idingreso = $ingreso->id;
+                $detalle->idarticulo = $det['idarticulo'];
+                $detalle->cantidad = $det['cantidad'];
+                $detalle->precio = $det['precio'];          
+                $detalle->save();
+            }
+            
+            DB::commit();
+        } catch (Exception $e){
+            DB::rollBack();
+        }
+    }
+    public function prueba(Request $request)
+    {
+        $fecha= Carbon::now('America/Guatemala');
+        $mes = $fecha->month;
+        $dia = $fecha->day;
+        $año = $fecha->year;
+
+        $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+
+        $mes2 = $meses[($fecha->format('n')) - 1];
+
+        return $dia.$mes2.$año;
+    }   
+
+    public function desactivar(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+        $ingreso = Ingreso::findOrFail($request->id);
+        $ingreso->estado = 'Anulado';
+        $ingreso->save();
+    }
+}
